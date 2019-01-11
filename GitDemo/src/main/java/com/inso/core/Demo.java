877f5470b4130cd8762edd278b93515bc424c7cb
eason <eason@inso.com>
2019-01-11 16:51:35 +0800

    b第er次提交

--- GitDemo/src/main/java/com/inso/core/Demo.java
+++ GitDemo/src/main/java/com/inso/core/Demo.java
@@ -4,5 +4,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("a-1");
         System.out.println("b-1");
+        System.out.println("b-2");
     }
 }
